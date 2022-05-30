857b4e060d4febadad760cdff7dad8d7cb614b00
Pierre Colle <piercus@gmail.com>
2022-05-30 16:06:38 +0200

    fix: maxIter to maxIteration

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ class Ict {
 		estimate,
 		init = null,
 		threshold = null,
-		maxIter = 10,
+		maxIteration = 10,
 		logger = defaultLogger,
 	}) {
 		this.match = match;
 		this.estimate = estimate;
 		this.threshold = threshold;
-		this.maxIter = maxIter;
+		this.maxIteration = maxIteration;
 		this.init = init;
 		this.logger = logger;
 	}
@@ -30,22 +30,31 @@ class Ict {
 		});
 	}
 
-	recursiveRun({source, destination, iteration, cost, state}) {
-		if ((typeof (this.threshold) === 'number' && cost < this.threshold) || (typeof (this.maxIter) === 'number' && iteration >= this.maxIter)) {
-			return Object.assign({iteration}, state);
+	recursiveRun({source, destination, iteration, cost, state, bestState = null, bestCost = null, bestIteration = null}) {
+		if ((typeof (this.threshold) === 'number' && cost < this.threshold) || (typeof (this.maxIteration) === 'number' && iteration >= this.maxIteration)) {
+			return Object.assign({iteration, cost: bestCost, bestIteration}, bestState);
 		}
 
-		return this.iter({source, destination, state, iteration}).then(iterOutput => {
+		return this.iter({source, destination, state, iteration, bestState: bestState || state}).then(iterOutput => {
 			const newCost = iterOutput.cost;
-			this.logger.debug(`${iteration} cost: ${newCost}`);
-			return this.recursiveRun({source, destination, iteration: iteration + 1, cost: newCost, state: Object.assign({}, state, iterOutput)});
+			const newState = Object.assign({}, state, iterOutput);
+
+			if (bestCost === null || newCost < bestCost) {
+				bestCost = newCost;
+				bestIteration = iteration;
+				bestState = newState;
+				this.logger.debug(`At iteration ${iteration} best cost reached : ${newCost}`);
+			}
+
+			return this.recursiveRun({source, destination, iteration: iteration + 1, cost: newCost, state: newState, bestState, bestIteration, bestCost});
 		});
 	}
 
-	iter({source, destination, state, iteration}) {
-		return Promise.resolve(this.match({source, destination, state, logger: this.logger, iteration})).then(matchOutput => {
+	iter({source, destination, state, iteration, bestState}) {
+		return Promise.resolve(this.match({source, destination, state, bestState, logger: this.logger, iteration})).then(matchOutput => {
 			const {assignement, cost} = matchOutput;
-			const estimateInput = Object.assign({source, destination, state, logger: this.logger, iteration}, matchOutput);
+			this.logger.debug(`Matching ${iteration} cost: ${cost}`);
+			const estimateInput = Object.assign({source, destination, state, bestState, logger: this.logger, iteration}, matchOutput);
 			return Promise.resolve(this.estimate(estimateInput)).then(transformation => Object.assign({cost, assignement}, transformation));
 		});
 	}
